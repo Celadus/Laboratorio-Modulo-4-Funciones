787118838fed208e3d0ea31a0ff55fba96c06a54
Matteo Zamana <119547209+Celadus@users.noreply.github.com>
2023-05-26 01:56:09 +0200

    anadido button reset

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+
+/*
+<body>
+
+<h1>JavaScript is powerful</h1>
+
+<p>JavaScript can change the HTML element:</p>
+
+<button type="button" onclick="myFunction()">Click Me!</button>
+
+<p id="power_of_javascript_demo">The power of Javascript.</p>
+
+<script>
+function myFunction() { 
+    document.getElementById("power_of_javascript_demo").innerHTML = "JavaScript, you're powerful!";
+}
+</script>
+
+</body>
+</html> */

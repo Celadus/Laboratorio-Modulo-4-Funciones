ea8220fe635caa709f177219f4b0037e3b6815b2
Matteo Zamana <119547209+Celadus@users.noreply.github.com>
2023-05-28 01:08:28 +0200

    anadido nuevo buton para elegir el numero de turno i selectarlo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@
   let counterMinusElem = (document.querySelector(".indietro")as HTMLInputElement);
   let counterResetElem = (document.querySelector(".reset")as HTMLInputElement);
   let counterPlusElem = (document.querySelector(".avanti")as HTMLInputElement);
+  //nuevo boton//
+  let inputTurnoElem = (document.querySelector(".input-turno")as HTMLInputElement);
+  let cambiarTurnoElem = (document.querySelector(".cambiar-turno")as HTMLInputElement);
 
   let contatore = 1;
 
@@ -24,6 +27,13 @@
     }
     updateDisplay();
   });
+
+  cambiarTurnoElem.addEventListener("click", () => {
+    const nuevoTurno = parseInt(inputTurnoElem.value);
+      contatore = nuevoTurno;
+      updateDisplay();
+  });
+      
 
   function updateDisplay() {
     counterDisplayElem.textContent = contatore.toString().padStart(2, "0");
